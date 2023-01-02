d9e4a29114bf0bd10e64cc99efa14afc98250a22
Sall <sall@w-ss.io>
2023-01-02 08:31:11 +0000

    Update zsh-fancy-completions.plugin.zsh
    Signed-off-by: Sall <sall@w-ss.io>

--- zsh-fancy-completions.plugin.zsh
+++ zsh-fancy-completions.plugin.zsh
@@ -1,11 +1,11 @@
 # -*- mode: zsh; sh-indentation: 2; indent-tabs-mode: nil; sh-basic-offset: 2; -*-
 # vim: ft=zsh sw=2 ts=2 et
+
 # Return if requirements are missing
 if [[ $TERM == 'dumb' ]]; then
   return 1
 fi
 
-# Zsh Plugin Standard
 # https://wiki.zshell.dev/community/zsh_plugin_standard#zero-handling
 0="${ZERO:-${${0:#$ZSH_ARGZERO}:-${(%):-%N}}}"
 0="${${(M)0:#/*}:-$PWD/$0}"
@@ -19,6 +19,4 @@ if [[ $PMSPEC != *f* ]]; then
   fpath+=( "${0:h}/functions" )
 fi
 
-autoload -Uz .completion-tab
-
-zmodload -i zsh/complist && .completion-tab
+source ${0:h}/functions/.completion-tab
