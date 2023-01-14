5ef0c80d4e3a163d4ca220d89c5066a6f07d04eb
Sall <sall@w-ss.io>
2023-01-14 16:09:38 +0000

    Update options.zsh
    Signed-off-by: Sall <sall@w-ss.io>

--- lib/options.zsh
+++ lib/options.zsh
@@ -3,13 +3,20 @@
 #
 # Description: Options
 
-
 setopt COMPLETE_IN_WORD    # Complete from both ends of a word.
 setopt ALWAYS_TO_END       # Move cursor to the end of a completed word.
 setopt PATH_DIRS           # Perform path search even on command names with slashes.
 setopt AUTO_MENU           # Show completion menu on a successive tab press.
 setopt AUTO_LIST           # Automatically list choices on ambiguous completion.
 setopt AUTO_PARAM_SLASH    # If completed parameter is a directory, add a trailing slash.
-setopt EXTENDED_GLOB        # Needed for file modification glob modifiers with compinit.
+setopt EXTENDED_GLOB       # Needed for file modification glob modifiers with compinit.
 unsetopt MENU_COMPLETE     # Do not autoselect the first completion entry.
 unsetopt FLOW_CONTROL      # Disable start/stop characters in shell editor.
+
+# Adjust zsh-users/zsh-autosuggest
+typeset -g ZSH_AUTOSUGGEST_USE_ASYNC=true
+typeset -g ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE=20
+typeset -g ZSH_AUTOSUGGEST_HISTORY_IGNORE="?(#c100,)"
+typeset -g ZSH_AUTOSUGGEST_COMPLETION_IGNORE="[[:space:]]*"
+typeset -g ZSH_AUTOSUGGEST_MANUAL_REBIND=set
+typeset -g ZSH_AUTOSUGGEST_STRATEGY=(history completion)
