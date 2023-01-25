336735baa6c354286abff35cebb1af73ea1b321c
Sall <sall@w-ss.io>
2023-01-25 23:45:25 +0000

    Update completion.zsh
    Signed-off-by: Sall <sall@w-ss.io>

--- lib/completion.zsh
+++ lib/completion.zsh
@@ -1,8 +1,10 @@
 # -*- mode: zsh; sh-indentation: 2; indent-tabs-mode: nil; sh-basic-offset: 2; -*-
 # vim: ft=zsh sw=2 ts=2 et
 
-
 # ‑‑‑‑‑‑‑‑‑ ⸨ COMPLETION ⸩ ‑‑‑‑‑‑‑‑‑‑‑‑‑‑‑‑‑‑
+
+# Automatically load bash completion functions
+autoload -U +X bashcompinit && bashcompinit
 
 # Enable cache - Some functions, like _apt and _dpkg, are very slow.
 # You can use a cache in order to proxy the list of results (like the list of available debian packages)
@@ -39,6 +41,23 @@ zstyle ':completion:*' verbose yes
 
 # Enable history menu selection
 zstyle ':completion:*:history-words'	menu yes
+
+# Enable completion of 'cd -<tab>' and 'cd -<ctrl-d>' with menu
+zstyle ':completion:*:*:cd:*' tag-order local-directories directory-stack path-directories
+zstyle ':completion:*:*:cd:*:directory-stack' menu yes select
+
+# Enable expand completer for all expansions
+zstyle ':completion:*:expand:*'		tag-order all-expansions
+zstyle ':completion:*:history-words'	list false
+
+# Enable offering indexes before parameters in subscripts
+zstyle ':completion:*:*:-subscript-:*'	tag-order indexes parameters
+
+# Enable matches to separate into groups
+zstyle ':completion:*:matches'		group 'yes'
+
+# Enable processes completion for all user processes
+zstyle ':completion:*:processes'	command 'ps -au$USER'
 
 # Fuzzy match mistyped completions.
 zstyle ':completion:*' completer _complete _match _approximate
@@ -80,9 +99,9 @@ zstyle ':completion:*' special-dirs true
 # Kill
 zstyle ':completion:*:*:*:*:processes' command 'ps -u $USER -o pid,user,comm -w'
 zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;36=0=01'
-zstyle ':completion:*:*:kill:*' menu yes select
-zstyle ':completion:*:*:kill:*' force-list always
-zstyle ':completion:*:*:kill:*' insert-ids single
+zstyle ':completion:*:*:(killall|pkill|kill):*' menu yes select
+zstyle ':completion:*:*:(killall|pkill|kill):*' force-list always
+zstyle ':completion:*:*:(killall|pkill|kill):*' insert-ids single
 
 # Man
 zstyle ':completion:*:manuals' separate-sections true
@@ -134,5 +153,42 @@ zstyle '*' single-ignored show
 zstyle ':completion:*:(rm|kill|diff):*' ignore-line other
 zstyle ':completion:*:rm:*' file-patterns '*:all-files'
 
-# Automatically load bash completion functions
-autoload -U +X bashcompinit && bashcompinit
+# Prevent CVS files/directories from being completed
+zstyle ':completion:*:(all-|)files'	ignored-patterns '(|*/)CVS'
+zstyle ':completion:*:cd:*'		ignored-patterns '(*/)#CVS'
+
+# Prevent commands like `rm'
+zstyle ':completion:*:rm:*'		ignore-line yes
+
+# Prevent menu completion for ambiguous initial strings
+zstyle ':completion:*'			insert-unambiguous true
+zstyle ':completion:*:corrections'	format $'%{\e[0;31m%}%d (errors: %e)%{\e[0m%}'
+zstyle ':completion:*:correct:*'	original true
+
+# Prevent men completion for duplicate entries
+zstyle ':completion:*:history-words'	remove-all-dups yes
+zstyle ':completion:*:history-words'	stop yes
+
+# Prevent completion of functions for commands you don't have
+zstyle ':completion:*:functions'	ignored-patterns '(_*|pre(cmd|exec))'
+
+# Prevent files to be ignored from zcompile
+zstyle ':completion:*:*:zcompile:*'	ignored-patterns '(*~|*.zwc)'
+zstyle ':completion:correct:'		prompt 'correct to: %e'
+
+#Prevent comp to glob the first part of the path to avoid partial globs. (Performance)
+zstyle ':completion:*' accept-exact '*(N)'
+
+# Prevent trailing slash if argument is a directory
+zstyle ':completion:*'			squeeze-slashes true
+zstyle ':completion::complete:*' '\\'
+
+# Prevent completion of backup files as executables
+zstyle ':completion:*:complete:-command-::commands' ignored-patterns '*\~'
+
+# Prevent these filename suffixes to ignore during completion (except after rm command)
+zstyle ':completion:*:*:(^rm):*:*files' ignored-patterns  '*?.(o|c~|old|pro|zwc)' '*~'
+
+# Prevent rm completion
+zstyle ':completion:*:(rm|kill|diff):*' ignore-line other
+zstyle ':completion:*:rm:*' file-patterns '*:all-files'
