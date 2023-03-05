a2388ae9840abe60090d17fb4b04a84062bcb29b
Sall <sall@w-ss.io>
2023-03-05 17:08:09 +0000

    Update zsh-fancy-completions.plugin.zsh
    Signed-off-by: Sall <sall@w-ss.io>

--- zsh-fancy-completions.plugin.zsh
+++ zsh-fancy-completions.plugin.zsh
@@ -1,11 +1,6 @@
 # -*- mode: zsh; sh-indentation: 2; indent-tabs-mode: nil; sh-basic-offset: 2; -*-
 # vim: ft=zsh sw=2 ts=2 et
 
-# Return if requirements are missing
-if [[ $TERM == 'dumb' ]]; then
-  return 1
-fi
-
 # https://wiki.zshell.dev/community/zsh_plugin_standard#zero-handling
 0="${ZERO:-${${0:#$ZSH_ARGZERO}:-${(%):-%N}}}"
 0="${${(M)0:#/*}:-$PWD/$0}"
@@ -19,6 +14,11 @@ if [[ $PMSPEC != *f* ]]; then
   fpath+=( "${0:h}/functions" )
 fi
 
-source ${0:h}/lib/options.zsh
-source ${0:h}/lib/completion.zsh
-source ${0:h}/lib/tab-completion.zsh
+# Return if requirements are missing
+if [[ $TERM == 'dumb' ]]; then
+  return 0
+else
+  source ${0:h}/lib/options.zsh
+  source ${0:h}/lib/completion.zsh
+  source ${0:h}/lib/tab-completion.zsh
+fi
